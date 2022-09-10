a0f55a95d63c15e698ddaeefd71b5dbc3392c40a
Lucas NG <3543285+lucasng-dev@users.noreply.github.com>
2022-09-09 22:58:57 -0300

    WIP: Rebase project

--- src/main.rs
+++ src/main.rs
@@ -72,20 +72,20 @@ fn cores() {
 }
 
 fn conteudo_opcional() {
-  let conteudo_arquivo = let_arquivo(String::from(""));
+  let conteudo_arquivo = ler_arquivo(String::from(""));
 
   match &conteudo_arquivo {
     Some(valor) => println!("{}", valor),
-    None => println!("Arquivo não existe"),
-  }
+    None => println!("Arquivo nao existe"),
+  };
 
-  println!("{:?}", &conteudo_arquivo);
+  println!("{:?}", conteudo_arquivo);
 
   if let Some(valor) = conteudo_arquivo {
-    println!("{}", valor);
+    println!("Agora, tenho certeza de ha o valor {}", valor);
   }
 }
 
-fn let_arquivo(caminho: String) -> Option<String> {
-  Some(String::from("Conteúdo do arquivo"))
+fn ler_arquivo(caminho_arquivo: String) -> Option<String> {
+  Some(String::from("Algum conteudo"))
 }
